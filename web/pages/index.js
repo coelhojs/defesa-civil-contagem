cf42d2aa16213d40bb85c82c5ec8f692995db71d
coelhojs <ccoelho.me@gmail.com>
2019-09-02 18:58:20 -0300

    Iniciando interface web

---
+++ web/pages/index.js
@@ -0,0 +1,11 @@
+import Header from "../components/header";
+import UserForm from "../components/userForm";
+
+const Index = () => (
+    <div>
+        <Header></Header>
+        <UserForm></UserForm>
+    </div>
+);
+
+export default Index;
