bf396045259ab1126a3cc433dee562ff8c94f503
Caique <caiiquexd@gmail.com>
2019-09-05 19:49:52 -0300

    git merge

---
+++ mobile/lib/main.dart
@@ -0,0 +1,22 @@
+import 'dart:collection';
+
+import 'package:defesa_civil/ui/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: Home(),
+      theme: ThemeData(fontFamily: 'Uber'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
