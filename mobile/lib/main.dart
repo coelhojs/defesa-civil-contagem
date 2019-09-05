cdea11cfe332acf24db9c7c951d732ff827cac5e
Caique <caiiquexd@gmail.com>
2019-09-05 19:59:57 -0300

    Teste commit mobile
    Former-commit-id: 8476bc14f4756b796b1eb546d5827533495e0371

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Google Maps Demo',
+
       home: Home(),
       theme: ThemeData(fontFamily: 'Uber'),
       debugShowCheckedModeBanner: false,
