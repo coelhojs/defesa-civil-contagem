26d51910f80cf03661ae716f5fdb5bbed196446b
Caique <caiiquexd@gmail.com>
2019-09-05 22:13:47 -0300

    Adicionado começo de login com google
    Adicionado começo de login com google
    
    Former-commit-id: 013fb7c97b53fb0e2d263e945f6d7ad34794cbd3

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Google Maps Demo',
-
       home: Home(),
       theme: ThemeData(fontFamily: 'Uber'),
       debugShowCheckedModeBanner: false,
--- mobile/lib/ui/registerpage.dart
+++ mobile/lib/ui/registerpage.dart
@@ -1,6 +1,9 @@
+import 'package:defesa_civil/ui/loginpage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+import '../sign_in.dart';
 import '../size_config.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -9,7 +12,6 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -20,51 +22,136 @@ class _RegisterPageState extends State<RegisterPage> {
       ),
       backgroundColor: Colors.white,
       body: Container(
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-
               Padding(
-                padding: EdgeInsets.only(top: 5, bottom: 20),
+                padding: EdgeInsets.only(bottom: 0),
                 child: Image(
                     height: SizeConfig.blockSizeHorizontal * 40,
                     image: AssetImage("assets/images/defesa_civil.jpg")),
               ),
               Align(
-                child: Text("Cadastre-se", style: TextStyle(fontSize: SizeConfig.blockSizeHorizontal *8,fontWeight: FontWeight.w500),),
+                child: Text(
+                  "Cadastre-se",
+                  style: TextStyle(
+                      fontSize: SizeConfig.blockSizeHorizontal * 7,
+                      fontWeight: FontWeight.w500),
+                ),
                 alignment: Alignment.topLeft,
               ),
-              Padding(padding: EdgeInsets.all(10),),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              _signInButton(),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              TextFormField(
+                maxLength: 11,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.assignment_ind),
+                  labelText: 'CPF:',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5),
+              ),
+              TextFormField(
+                maxLength: 11,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.phone),
+                  labelText: 'Telefone:',
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text("Endereço"),
+              ),
+              Padding(
+                padding: EdgeInsets.all(3),
+              ),
               TextFormField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.person),
-                  hintText: 'Digite o nome para login!',
-                  labelText: 'Nome:',
+                  prefixIcon: Icon(Icons.streetview),
+                  labelText: 'Rua:',
                 ),
               ),
-              Padding(padding: EdgeInsets.all(20),),
+              Padding(
+                padding: EdgeInsets.all(3),
+              ),
               TextFormField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.lock),
-                  labelText: 'Senha:',
+                  prefixIcon: Icon(Icons.streetview),
+                  labelText: 'Nº:',
                 ),
-                obscureText: true,
               ),
-              Padding(padding: EdgeInsets.all(10),),
+              Padding(
+                padding: EdgeInsets.all(3),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.streetview),
+                  labelText: 'Bairro:',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(3),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.streetview),
+                  labelText: 'Cidade:',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(3),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.streetview),
+                  labelText: 'CEP:',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(3),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.streetview),
+                  labelText: 'Complemento:',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
               Align(
                 child: FlatButton(
                   child: Container(
-                    height: SizeConfig.blockSizeVertical*5,
-                    width: SizeConfig.blockSizeHorizontal*25,
+                    height: SizeConfig.blockSizeVertical * 7,
+                    width: SizeConfig.blockSizeHorizontal * 25,
                     decoration: BoxDecoration(
                       color: Colors.green,
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
                     child: Center(
-                      child: Text("Cadastrar", style: TextStyle(color: Colors.white, fontSize: SizeConfig.blockSizeHorizontal * 4),),
+                      child: Text(
+                        "Cadastrar",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: SizeConfig.blockSizeHorizontal * 4),
+                      ),
                     ),
                   ),
                 ),
@@ -77,4 +164,43 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/images/google.jpg"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      onPressed: () {
+        signInWithGoogle().whenComplete(() {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return LoginPage();
+              },
+            ),
+          );
+        });
+      },
+    );
+  }
 }
