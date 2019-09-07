d22226ec943e3856671147004dffe994cf6747ff
coelhojs <ccoelho.me@gmail.com>
2019-09-07 02:05:51 -0300

    geolocation funcionando no webapp
    Former-commit-id: 03f6687a7e067299a4139920f5564a57db59878d

--- web/src/components/map.js
+++ web/src/components/map.js
@@ -1,62 +1,41 @@
-import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-// import Loader from "./loader";
-// const Geolocation = require('../controllers/Geolocation');
+import React, { Component } from 'react';
+import { geolocated } from "react-geolocated";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Map extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { center: this.getLocation() };
-    }
-
-    getLocation() {
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                let center = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                return center;
-
-            });
-        } else {
-            return "Localização desabilitada pelo usuário";
-        }
-    }
-
     // https://maps.googleapis.com/maps/api/geocode/json?language=ja&sensor=false&latlng=35.6909389,139.6952959&key=AIzaSyDrud8AunST8uTxV0K7df50dFjnM59kbBg
 
-    // static defaultProps = {
-    //     center: {
-    //         lat: 59.95,
-    //         lng: 30.33
-    //     },
-    //     zoom: 11
-    // };
-
     render() {
-        console.log(this.state);
-        
-        return (
-            // Important! Always set the container height explicitly
+        return !this.props.isGeolocationAvailable ? (
+            <div>Your browser does not support Geolocation</div>
+        ) : !this.props.isGeolocationEnabled ? (
+            <div>Geolocation is not enabled</div>
+        ) : this.props.coords ? (
             <div style={{ height: '100vh', width: '100%' }} >
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: "AIzaSyDrud8AunST8uTxV0K7df50dFjnM59kbBg" }}
-                    defaultCenter={this.state.center}
+                    defaultCenter={[this.props.coords.latitude, this.props.coords.longitude]}
                     defaultZoom={11}
                 >
                     <AnyReactComponent
-                        lat={this.lat}
-                        lng={this.lng}
+                        lat={this.props.coords.latitude}
+                        lng={this.props.coords.longitude}
                         text="My Marker"
                     />
                 </GoogleMapReact>
             </div >
-        );
+        ) : (
+                        <div>Getting the location data&hellip; </div>
+                    );
     }
 }
 
-export default Map;
+
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+})(Map);
