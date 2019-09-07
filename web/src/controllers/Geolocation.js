4fe06cee594511c53f9f2a4ebdbbc6d203558298
coelhojs <ccoelho.me@gmail.com>
2019-09-07 02:05:51 -0300

    geolocation funcionando no webapp
    Former-commit-id: a7f6e8321156897412f36309543a64f380ad71f8

--- web/src/controllers/Geolocation.js
+++ web/src/controllers/Geolocation.js
@@ -1,19 +1,46 @@
-function getLocation() {
-    if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            let center = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            return center;
-        });
-    } else {
-        return "Localização desabilitada pelo usuário";
-    }
+import React from "react";
+import { geolocated } from "react-geolocated";
 
+class Geolocation extends React.Component {
+    render() {
+        return !this.props.isGeolocationAvailable ? (
+            <div>Your browser does not support Geolocation</div>
+        ) : !this.props.isGeolocationEnabled ? (
+            <div>Geolocation is not enabled</div>
+        ) : this.props.coords ? (
+            <table>
+                <tbody>
+                    <tr>
+                        <td>latitude</td>
+                        <td>{this.props.coords.latitude}</td>
+                    </tr>
+                    <tr>
+                        <td>longitude</td>
+                        <td>{this.props.coords.longitude}</td>
+                    </tr>
+                    <tr>
+                        <td>altitude</td>
+                        <td>{this.props.coords.altitude}</td>
+                    </tr>
+                    <tr>
+                        <td>heading</td>
+                        <td>{this.props.coords.heading}</td>
+                    </tr>
+                    <tr>
+                        <td>speed</td>
+                        <td>{this.props.coords.speed}</td>
+                    </tr>
+                </tbody>
+            </table>
+        ) : (
+                        <div>Getting the location data&hellip; </div>
+                    );
+    }
 }
 
-
-module.exports = {
-    getLocation: getLocation
-};
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+})(Geolocation);
