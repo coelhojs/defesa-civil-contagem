ed30bd95362d18ba563b591f6a813a5407ccf997
Lucas Laborne <lucas.laborne@gmail.com>
2019-09-08 14:09:37 -0300

    Adiçao de templates de Rotas
    Former-commit-id: 26f15b08b9d84774dadd03508ee8ab23b2006c0b

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const app = express();
 app.use(express.static('./public'));
 
-
 // Middlewares:
 app.use(cors());
 app.use(morgan('dev'));
@@ -15,6 +14,14 @@ app.use(bodyparser.urlencoded({ extended: false }));
 
 // Rotas
 app.get('/', (req, res, next) => res.status(200).redirect('/index.html'));
+
+// Entidades Fisicas:
+app.use('/usuarios', require('./rotas/usuario/usuario.rotas'));
+app.use('/chamados', require('./rotas/chamado/chamado.rotas'));
+
+// Blog e informativos:
+app.use('/informativos', require('./rotas/informativo/informativo.rotas'));
+app.use('/noticias', require('./rotas/noticia/noticia.rotas'));
 
 // Tratamento de rotas inválidas:
 app.use((req, res, next) => res.status(404).redirect('/notfound.html'));
