58546d7b07ea656dcfedc091905962ae9258e9f4
coelhojs <ccoelho.me@gmail.com>
2019-09-08 11:02:14 -0300

    ocultando mapa
    Former-commit-id: 2beda819ab449dc8e7549eca4c894b7aadb4f7b7

--- web/src/App.js
+++ web/src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import Header from "./components/header";
 import Map from "./components/map";
 import UserForm from "./components/userForm";
+import Drawer from './components/drawer';
 function App() {
   return (
     <div className="App">
       <Container maxWidth={false}>
         <Header></Header>
-        <Map></Map>
+        {/* <Map></Map> */}
         <UserForm></UserForm>
       </Container>
     </div>
