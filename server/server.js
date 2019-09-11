adb6fb16ab7627088f77a05f6779c218844ca350
lucas <lucas.laborne@gmail.com>
2019-09-10 21:26:37 -0300

    Adicionado servidor e banco de dados
    Former-commit-id: df8d5564876d9366763020311f189b2dcf44b462

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app');
+const db = require('./database');
+
+const PORTA = process.env.PORT || 3001;
+
+console.log('[%s] Estabelecendo conexão com o banco de dados...', (new Date).toLocaleString());
+db.init(() => {
+    console.log('[%s] Conexão com o banco de dados estabelecida. [%s]', (new Date).toLocaleString(), db.isLocal() ? 'LOCAL' : 'REMOTO');
+    http.createServer(app).listen(PORTA, () => {
+        console.log(`[%s] Servidor escutando na porta ${PORTA}...`, (new Date).toLocaleString());
+    });
+});
