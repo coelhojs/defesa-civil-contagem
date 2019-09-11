05f610d0c4bf0e44c941662424806d4df14411dd
lucas <lucas.laborne@gmail.com>
2019-09-10 21:26:37 -0300

    Adicionado servidor e banco de dados
    Former-commit-id: 89597bfaadc985696d3e1fd451839d3189ac9dcc

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const bodyparser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+app.use(express.static('./public'));
+
+// Middlewares:
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+// Rotas
+app.get('/', (req, res, next) => res.status(200).redirect('/index.html'));
+
+// Entidades Fisicas:
+app.use('/usuarios', require('./rotas/usuario.rotas'));
+app.use('/chamados', require('./rotas/chamado.rotas'));
+
+// Blog e informativos:
+app.use('/informativos', require('./rotas/informativo.rotas'));
+app.use('/noticias', require('./rotas/noticia.rotas'));
+
+// Tratamento de rotas inválidas:
+app.use((req, res, next) => res.status(404).redirect('/notfound.html'));
+
+module.exports = app;
