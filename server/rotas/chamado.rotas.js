d8acbe41bb836f0cd5d5004c838e70065eaacd5b
Lucas Laborne <lucas.laborne@gmail.com>
2019-09-13 08:01:40 -0300

    Arrumadas as rotas

--- server/rotas/chamado.rotas.js
+++ server/rotas/chamado.rotas.js
@@ -2,33 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 
-// Obtém todos os elementos:
-router.get('/all', (req, res, next) => {
+// Obtém o chamado do usuário atual:
+router.get('/get', (req, res) => {
 
 });
 
 // Pesquisa por 0,N elementos:
-router.get('/all', (req, res, next) => {
+router.get('/all', (req, res) => {
 
 });
 
 // Obtém um elemento específico:
-router.get('/:id', (req, res, next) => {
+router.get('/:id', (req, res) => {
 
 });
 
 // Adiciona um elemento:
-router.post('/', (req, res, next) => {
+router.post('/', (req, res) => {
 
 });
 
 // Modifica um elemento:
-router.put('/:id', (req, res, next) => {
+router.put('/:id', (req, res) => {
 
 });
 
 // Deleta um elemento:
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', (req, res) => {
 
 });
 
