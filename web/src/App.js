1fce12f7eb0935634471d764758b78f74cc3dd7f
coelhojs <ccoelho.me@gmail.com>
2019-09-14 17:34:59 -0300

    trabalhando no auth
    Former-commit-id: 4c1a826da96840aa41315ab041946dd8fba62bde

--- web/src/App.js
+++ web/src/App.js
@@ -1,40 +1,37 @@
+import Container from '@material-ui/core/Container';
+import { FirebaseAuthProvider } from "@react-firebase/auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
+import Header from './components/header';
 import Signin from "./components/signin";
 import Signup from "./components/signup";
 import Dashboard from './containers/Dashboard';
 import Inicio from './containers/Inicio';
-import firebase from "firebase/app";
-import "firebase/auth";
-import {
-  FirebaseAuthProvider,
-  FirebaseAuthConsumer,
-  IfFirebaseAuthed,
-  IfFirebaseAuthedAnd
-} from "@react-firebase/auth";
 import { firebaseConfig } from "./controllers/Auth";
 
-import { ProvideAuth } from "./controllers/Auth";
-
 function App() {
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
   return (
     <div className="App">
       <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
-        {/* <FirebaseAuthConsumer>
-          {({ isSignedIn, user, providerId }) => {
-            return (
-              <pre style={{ height: 300, overflow: "auto" }}>
-                {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
-              </pre>
-            );
-          }}
-        </FirebaseAuthConsumer> */}
-        <Switch>
-          <Route exact path="/" component={Inicio} />
-          <Route path="/Dashboard" component={Dashboard} />
-          <Route path="/Cadastro" component={Signup} />
-          <Route path="/Login" component={Signin} />
-        </Switch>
+        <Container maxWidth="sm">
+          <Header firebase={firebase}></Header>
+          <Switch>
+            <Route
+              path='/'
+              render={(props) => <Inicio
+                {...props}
+                firebase={firebase}
+                authProvider={googleAuthProvider} />}
+            />
+            <Route path="/Dashboard" component={Dashboard} />
+            {/* <Route path="/Cadastro" component={Signup} />
+            <Route path="/Login" component={Signin} /> */}
+          </Switch>
+        </Container >
       </FirebaseAuthProvider>
     </div>
   );
