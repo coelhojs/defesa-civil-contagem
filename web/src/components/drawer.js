48fd9d6ed2a7c56f8068b469d92c5d2084af0318
coelhojs <ccoelho.me@gmail.com>
2019-09-16 01:17:31 -0300

    rotas de chamados

--- web/src/components/drawer.js
+++ web/src/components/drawer.js
@@ -1,18 +1,35 @@
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 import MailIcon from '@material-ui/icons/Mail';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import React from 'react';
-import { classes } from '../styles';
+import { useAuth } from "../controllers/Auth";
 
-export default function ClippedDrawer() {
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    toolbar: theme.mixins.toolbar
+
+}));
+
+
+export default function AppDrawer() {
+    const classes = useStyles();
+    const auth = useAuth();
+
+    if (auth.user) {
+        return (
             <Drawer
                 className={classes.drawer}
                 variant="permanent"
@@ -30,6 +47,8 @@ export default function ClippedDrawer() {
                     ))}
                 </List>
             </Drawer>
-        </div>
-    );
+        )
+    } else {
+        return null;
+    }
 }
--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,27 +1,33 @@
-import { Link } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import React, { useState } from 'react';
-import { classes } from '../styles';
+import React, { useState } from 'react'; import { makeStyles } from '@material-ui/core/styles';
 import SignOutBtn from './signOutBtn';
 
+const useStyles = makeStyles(theme => ({
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    link: {
+        color: 'white'
+    }
+}));
+
 export default function Header(props) {
-    const [auth] = useState(props.auth);
-    const [firebase] = useState(props.firebase);
+    const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar position="fixed" className={classes.appBar}>
-                <Toolbar>
-                    <Typography variant="h6" noWrap>
-                        <Link to="/">Defesa Civil de Contagem</Link>
-                    </Typography>
-                    <SignOutBtn auth={auth} firebase={firebase} />
-                </Toolbar>
-            </AppBar>
-        </div>
+        <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+                <Typography variant="h6" noWrap>
+                    <Link component={RouterLink} to="/" className={classes.link}>
+                        Defesa Civil de Contagem
+                    </Link>
+                </Typography>
+                <SignOutBtn />
+            </Toolbar>
+        </AppBar>
     );
 }
