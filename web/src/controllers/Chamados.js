58bfc8f5dc7b5d66a4869a642970807833871c32
coelhojs <ccoelho.me@gmail.com>
2019-09-16 16:30:02 -0300

    exibindo lista de chamados

--- web/src/controllers/Chamados.js
+++ web/src/controllers/Chamados.js
@@ -1,7 +1,18 @@
-import history from '../history';
+import { useState, useEffect } from 'react';
 import { api } from "./index";
 
-export const getAllChamados = () => async dispatch => {
-    const response = await api.get('/Chamados');
-    dispatch({ type: "GET", payload: response.data });
+export const useFetchAllChamados = chamado => {
+    const [chamados, setChamados] = useState([]);
+
+    useEffect(
+        () => {
+            (async chamado => {
+                const response = await api.get('/Chamados');
+                setChamados(response.data);
+            })(chamado);
+        },
+        [chamado]
+    );
+
+    return chamados;
 };
