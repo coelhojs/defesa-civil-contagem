46c508200974aee4a1ac3c7b299a630365d4a6c9
coelhojs <ccoelho.me@gmail.com>
2019-09-23 18:55:26 -0300

    trabalhando com mapas
    Former-commit-id: 4a9e7ee4f4cf984709ba42cf61667133ef578860

--- web/src/containers/Mapa.js
+++ web/src/containers/Mapa.js
@@ -1,67 +1,29 @@
 //https://cherniavskii.com/using-leaflet-in-react-apps-with-react-hooks/
 import React, { useState, useEffect, useRef } from "react";
-import L from "leaflet";
+import { makeStyles } from '@material-ui/core/styles';
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
-function Map() {
-    const style = {
-        width: "300px",
-        height: "300px"
-    };
-
-    const [markersData, setMarkersData] = useState([
-        { latLng: { lat: -19.919387, lng: -44.080628 }, title: 1 }
-    ]);
-
-    function addMarker() {
-        const lastMarker = markersData[markersData.length - 1];
-
-        setMarkersData([
-            ...markersData,
-            {
-                title: +lastMarker.title + 1,
-                latLng: {
-                    lat: lastMarker.latLng.lat + 0.0001,
-                    lng: lastMarker.latLng.lng + 0.0001
-                }
-            }
-        ]);
+const useStyles = makeStyles(theme => ({
+    leafletContainer: {
+        height: '400px',
+        width: '80%',
+        margin: '0 auto',
     }
 
-    // create map
-    const mapRef = useRef(null);
-    useEffect(() => {
-        mapRef.current = L.map("map", {
-            center: [49.8419, 24.0315],
-            zoom: 16,
-            layers: [
-                L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-                    attribution:
-                        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                })
-            ]
-        });
-    }, []);
+}));
 
-    // add layer
-    const layerRef = useRef(null);
-    useEffect(() => {
-        layerRef.current = L.layerGroup().addTo(mapRef.current);
-    }, []);
-
-    // update markers
-    useEffect(
-        () => {
-            layerRef.current.clearLayers();
-            markersData.forEach(marker => {
-                L.marker(marker.latLng, { title: marker.title }).addTo(
-                    layerRef.current
-                );
-            });
-        },
-        [markersData]
-    );
-
-    return <div id="map" style={style} />;
+export default function Mapa() {
+    const classes = useStyles();
+    const center = [51.505, -0.09];
+    return (
+        <Map center={center} zoom={13}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={center}>
+                <Popup>A simple popup</Popup>
+            </Marker>
+        </Map>
+    )
 }
-
-export default Map;
