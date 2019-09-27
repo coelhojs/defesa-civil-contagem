e7fca54c1997719340de531c2da22186ac30e7d5
coelhojs <ccoelho.me@gmail.com>
2019-09-26 21:19:25 -0300

    corrigindo bug do leaflet
    Former-commit-id: ea2314650c7c426a09425d7163f44a2d5eac1c1d

--- web/src/containers/Mapa.js
+++ web/src/containers/Mapa.js
@@ -1,29 +1,30 @@
 //https://cherniavskii.com/using-leaflet-in-react-apps-with-react-hooks/
 import React, { useState, useEffect, useRef } from "react";
+import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 const useStyles = makeStyles(theme => ({
     leafletContainer: {
         height: '400px',
-        width: '80%',
-        margin: '0 auto',
+        width: '100%'
     }
-
 }));
 
 export default function Mapa() {
     const classes = useStyles();
     const center = [51.505, -0.09];
     return (
-        <Map center={center} zoom={13}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <Marker position={center}>
-                <Popup>A simple popup</Popup>
-            </Marker>
-        </Map>
+        <Container maxWidth="lg" fixed>
+            <Map center={center} zoom={13} className={classes.leafletContainer}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={center}>
+                    <Popup>A simple popup</Popup>
+                </Marker>
+            </Map>
+        </Container>
     )
 }
