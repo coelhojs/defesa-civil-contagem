196cd0a9f753fc33af378cf0903363570a4cbc65
Lucas <lucas.laborne@gmail.com>
2019-09-30 12:03:25 -0300

    update-sv
    Former-commit-id: 5f16c473b1e70d8e3b5a10a26ecccc4423ba2e37

---
+++ server/dev.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/status', (req, res) => {
+	res.status(200).json({
+		status: 'online',
+		versão: 'v2.0'
+	});
+});
+
+module.exports = router;
