5a83931094061154568164e2981eb65e259fb714
Lucas Laborne <lucas.laborne@gmail.com>
2019-09-30 10:16:24 -0300

    Reformulada a autorização, removidos pacotes desnecessarios
    Former-commit-id: 631d7c98ef497133481e68cd3fb3dd0874b28643

--- server/auth/authorization.js
+++ server/auth/authorization.js
@@ -1,8 +1,46 @@
+const Usuario = require('../modelos/usuario.modelo');
 const { AppError } = require('../handlers/error');
-const express = require('express');
+const JWT = require('jsonwebtoken');
 
-const router = express.Router();
+// Senha para codificar/decodificar o id de usuários:
+const SECRET = 'roundrobin1234';
 
+function gerarApiKey(user_id) {
+	return JWT.sign(user_id, SECRET);
+}
 
+// Controla todas as rotas de acesso
+async function acesso(req, res, next) {
+	try {
+		// Decodifica a api_key recebida no cabeçalho e obtem o user_id:
+		let user_id = JWT.verify(req.headers.authorization.split(' ')[1].trim(), SECRET);
+		// Procura um usuário com o user_id correspondente:
+		let user = await Usuario.findById(user_id);
+		// Se não encontrado, retorna um erro:
+		if (!user) throw new AppError({
+			http_cod: 401,
+			mensagem: 'Usuário não identificado',
+			mensagem_amigavel: 'Acesso não autorizado',
+		});
+		// Acopla o user_id e prossegue com a requisição:
+		req.user = {
+			id: user_id,
+		}
+		next();
 
-module.exports = router;
+	} catch (ex) {
+		if (ex instanceof AppError) next(ex);
+		else {
+			next(new AppError({
+				http_cod: 401,
+				mensagem: ex.message,
+				mensagem_amigavel: 'Acesso não autorizado',
+			}));
+		}
+	}
+}
+
+module.exports = {
+	gerarApiKey: gerarApiKey,
+	acesso: acesso,
+};
