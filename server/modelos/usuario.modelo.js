d5ce6897307035329e881ec3fc82bb7260292c23
Lucas <lucas.laborne@gmail.com>
2019-09-30 15:21:34 -0300

    Ajustada rota de chamados
    Former-commit-id: 22afd28a04eab1edf8a6ddb2079fbc622c9360c4

--- server/modelos/usuario.modelo.js
+++ server/modelos/usuario.modelo.js
@@ -18,4 +18,17 @@ const schema = new Schema({
 
 schema.plugin(muv);
 
+// Retorna apenas os atributos visíveis do modelo:
+schema.methods.format = function () {
+	return {
+		tipo: this.tipo,
+		nome: this.nome,
+		telefone: this.telefone,
+		cpf: this.cpf,
+		email: this.email,
+		nascimento: this.nascimento,
+		endereco: this.endereco,
+	}
+}
+
 module.exports = mongoose.model(NOME_MODELO, schema);
