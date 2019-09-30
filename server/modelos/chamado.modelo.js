d2d1cfd30a3097f5ae0877191ca7003a5f09b690
Lucas <lucas.laborne@gmail.com>
2019-09-30 15:40:19 -0300

    Ajustado modelo de chamados --update-sv
    Former-commit-id: 2b292b18d3d215cff0eed35fe189050b7ec1f4c5

--- server/modelos/chamado.modelo.js
+++ server/modelos/chamado.modelo.js
@@ -5,17 +5,30 @@ const Schema = mongoose.Schema;
 const NOME_MODELO = 'Chamado';
 
 const schema = new Schema({
-    user_id: { type: String },
-    tipo: { type: String },
-    dataHora: { type: Date },
-    descricao: { type: String },
-    foto: { type: String },
-    vistoriador: { type: String },
-    cidadao: { type: String },
-    local: { type: String },
+	user_id: { type: String },
+	tipo: { type: String },
+	dataHora: { type: Date },
+	descricao: { type: String },
+	foto: { type: String },
+	vistoriador: { type: String },
+	cidadao: { type: String },
+	local: { type: String },
 
 }, { strict: false });
 
 schema.plugin(muv);
+
+// Retorna apenas os atributos visíveis do modelo:
+schema.methods.toJSON = function () {
+	return {
+		tipo: this.tipo,
+		dataHora: this.dataHora,
+		descricao: this.descricao,
+		foto: this.foto,
+		vistoriador: this.vistoriador,
+		cidadao: this.cidadao,
+		local: this.local,
+	}
+}
 
 module.exports = mongoose.model(NOME_MODELO, schema);
