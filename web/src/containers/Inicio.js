741621daa86a9ef3c2f389c20bfc7fca85fca809
coelhojs <ccoelho.me@gmail.com>
2019-09-29 23:19:20 -0300

    corrigindo web
    Former-commit-id: c6a43fac0d227ee071e49b6da9265a8940082606

--- web/src/containers/Inicio.js
+++ web/src/containers/Inicio.js
@@ -1,17 +1,22 @@
 import React from 'react'; import { makeStyles } from '@material-ui/core/styles';
 import Login from "../components/login";
+import { useAuth } from "../customHooks/useAuth";
 
 const useStyles = makeStyles(theme => ({
-
 }));
 
 
 export default function Inicio(props) {
     const classes = useStyles();
+    const auth = useAuth();
 
-    return (
-        <div className={classes.content}>
-            <Login auth={props.auth} firebase={props.firebase} authProvider={props.authProvider} />
-        </div>
-    );
+    if (auth.user) {
+        return null
+    } else {
+        return (
+            <div className={classes.content}>
+                <Login auth={props.auth} firebase={props.firebase} authProvider={props.authProvider} />
+            </div>
+        )
+    }
 }
