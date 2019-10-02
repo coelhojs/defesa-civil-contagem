108c4b725eccedd74a8b8953cde1134f88d96651
Lucas Laborne <lucas.laborne@gmail.com>
2019-10-02 01:34:39 -0300

    Reestruturado o projeto --update-sv
    Former-commit-id: 998b13649e0ce8bdf5376fdf304efe9e662cb22a

--- server/dev.js
+++ server/dev.js
@@ -4,6 +4,8 @@ const Chamado = require('./modelos/chamado.modelo');
 const auth = require('./auth/authorization');
 const express = require('express');
 const moment = require('moment');
+const fs = require('fs');
+
 const router = express.Router();
 
 const tempoInicial = moment().format();
@@ -37,14 +39,22 @@ router.get('/usuarios', (req, res, next) => {
 		});
 });
 
-// Obtem usuarios (pode filtrar pela URL) com seus ids e api_keys
+// Cadastra um novo usuário
 router.post('/usuarios', async (req, res, next) => {
-	let user = new Usuario(req.body);
-	user.save();
-	let obj = user.toJSON();
-	obj.id = user.id;
-	obj.api_key = auth.gerarApiKey(user.id);
-	res.status(200).json(obj);
+	try {
+		let user = new Usuario(req.body);
+		user.save();
+		let obj = user.toJSON();
+		obj.id = user.id;
+		obj.api_key = auth.gerarApiKey(user.id);
+		res.status(200).json(obj);
+	} catch (ex) {
+		next(new AppError({
+			http_cod: 500,
+			mensagem: ex.message,
+			mensagem_amigavel: 'Erro ao cadastrar usuário',
+		}));
+	}
 });
 
 // Obtem chamados (pode filtrar pela URL)
@@ -59,6 +69,28 @@ router.get('/chamados', (req, res, next) => {
 				mensagem_amigavel: 'Erro ao recuperar informações de chamados',
 			}));
 		});
+});
+
+
+// CUIDADO: Remove TODOS os chamados que não possuem usuário atrelado e (opcionalmente) que não possuem imagens
+router.get('/chamados/sync', async (req, res, next) => {
+	try {
+		let chamados = await Chamado.find({});
+		for (let i = 0; i < chamados.length; i++) {
+			let c = chamados[i];
+			let user = await Usuario.findById(c.user_id);
+			if (!user || (req.query.nodir && !(fs.existsSync(`./imagens/${user.id}/${c.id}`)))) {
+				c.remove();
+			}
+		}
+		res.status(200).send('ok');
+	} catch (ex) {
+		next(new AppError({
+			http_cod: 500,
+			mensagem: ex.message,
+			mensagem_amigavel: 'Erro ao sincronizar chamados'
+		}));
+	}
 });
 
 module.exports = router;
