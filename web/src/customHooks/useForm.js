13062eaad278d98e4f0ae5ae34c0aa4f557f2bfe
MarcosHDBarbosa <marcos.hd.barbosa@gmail.com>
2019-10-03 22:05:08 -0300

    aatualiza form e novo buscacep
    Former-commit-id: 9baa01b9ef92c96ac36d25e83367936f5242ec72

--- web/src/customHooks/useForm.js
+++ web/src/customHooks/useForm.js
@@ -1,71 +1,103 @@
 //https://github.com/upmostly/custom-react-hooks-forms/blob/master/src/useForm.js
-import * as _ from 'lodash';
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { validarNome, validarTelefone, validarCPF, validarCEP } from '../validation/validateFormularios'
+import * as _ from "lodash";
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  validarNome,
+  validarTelefone,
+  validarCPF,
+  validarCEP,
+  validarNumero,
+  buscaCEP
+} from "../validation/validateFormularios";
 import { useAuth } from "./useAuth";
 
-
 export const useForm = (callback, inputs) => {
-    const auth = useAuth();
+  const auth = useAuth();
 
-    const [idToken, setIdToken] = useState('');
-    const [values, setValues] = useState(inputs);
-    const [errors, setErrors] = useState(inputs);
+  const [idToken, setIdToken] = useState("");
+  const [values, setValues] = useState(inputs);
+  const [errors, setErrors] = useState(inputs);
 
-    useEffect(() => {
-        if (auth.user) {
-            setValues({
-                ...values,
-                ["email"]:
-                    auth.user.email
-
-            });
-            // setValues(values => values.email = auth.user.email)
-        }
-    }, [auth.user]);
-
-
-    const handleSubmit = (event) => {
-        auth.signup(idToken, values);
-        if (event) event.preventDefault();
-        callback();
-    };
-
-    const handleChange = (event) => {
-        event.persist();
-        if ((event.target.name == "nome") && !(/[a-zA-ZÀ-ÿ]/.test(event.target.value) || event.target.value == ""))
-            return;
-        setValues(values => ({ ...values, [event.target.name]: event.target.value }))
-
-        switch (event.target.name) {
-            case "nome":
-                errors.nome = validarNome(values.nome);
-                break;
-            case "telefone":
-                errors.telefone = validarTelefone(values.telefone);
-                break;
-            case "cpf":
-                errors.cpf = validarCPF(values.cpf);
-                break;
-            case "cep":
-                errors.cep = validarCEP(values.cep);
-                //let resp = {erro, valores};
-                //console.log(resp);
-                //errors.cep = resp.erro;
-                //values = resp.values;
-                break;
-            default:
-                break;
-        }
-    };
-
-    const validaNome = (inpur) => {
-        //event.persist();
-        errors.nome = validarNome(inpur);
+  useEffect(() => {
+    if (auth.user) {
+      setValues({
+        ...values,
+        ["email"]: auth.user.email
+      });
+      // setValues(values => values.email = auth.user.email)
     }
+  }, [auth.user]);
 
-    /*const handleChange = (event) => {
+  const handleSubmit = event => {
+    auth.signup(idToken, values);
+    if (event) event.preventDefault();
+    callback();
+  };
+
+  const handleChange = event => {
+    event.persist();
+    if (
+      event.target.name == "nome" &&
+      !(/[a-zA-ZÀ-ÿ]/.test(event.target.value) || event.target.value == "")
+    )
+      return;
+    setValues(values => ({
+      ...values,
+      [event.target.name]: event.target.value
+    }));
+
+    switch (event.target.name) {
+      case "nome":
+        errors.nome = validarNome(values.nome);
+        break;
+      case "telefone":
+        errors.telefone = validarTelefone(values.telefone);
+        break;
+      case "cpf":
+        errors.cpf = validarCPF(values.cpf);
+        break;
+      case "numero":
+        errors.numero = validarNumero(values.numero);
+        break;
+      case "cep":
+        let resp = validarCEP(values.cep);
+        if (resp) errors.cep = resp;
+        else {
+          buscaCEP(values.cep.replace(/\D/g, ""))
+            .then(resolve => {
+              console.log(resolve);
+              values.logradouro = resolve.endereco.logradouro;
+              values.bairro = resolve.endereco.bairro;
+              values.cidade = resolve.endereco.localidade;
+              values.estado = resolve.endereco.uf;
+              errors.cep = resolve.error;
+            })
+            .catch(reject => {
+              errors.cep = "CEP invalido";
+            });
+        }
+        /*errors.cep = resp.error;
+                values.logradouro = resp.endereco.logradouro;
+                values.bairro = resp.endereco.bairro;
+                values.cidade = resp.endereco.cidade;
+                values.estado = resp.endereco.estado;*/
+        //let resp = {erro, valores};
+        //console.log(resp);
+        //errors.cep = resp.erro;
+        //values = resp.values;
+        break;
+      default:
+        break;
+    }
+  };
+
+  const validaNome = inpur => {
+    //event.persist();
+    errors.nome = validarNome(inpur);
+  };
+
+  /*const handleChange = (event) => {
         event.persist();
         setValues(values => ({ ...values, [event.target.name]: event.target.value }));
         switch (event.target.name) {
@@ -81,10 +113,10 @@ export const useForm = (callback, inputs) => {
         console.log(errors);
     };*/
 
-    return {
-        handleChange,
-        handleSubmit,
-        values,
-        errors
-    }
+  return {
+    handleChange,
+    handleSubmit,
+    values,
+    errors
+  };
 };
--- web/src/validation/validateFormularios.js
+++ web/src/validation/validateFormularios.js
@@ -1,185 +1,215 @@
-
-
-const required = value => value ? undefined : ' Campo obrigatório';
+const required = value => (value ? undefined : " Campo obrigatório");
 
 const defLength = (value, length) =>
-    value && value.length != length ? ` Este campo deve ter ${length} digitos ${value.length}` : undefined;
+  value && value.length != length
+    ? ` Este campo deve ter ${length} digitos ${value.length}`
+    : undefined;
 const maxLength = (value, max) =>
-    value && value.length > max ? ` O tamanho máximo para este campo é ${max}` : undefined;
+  value && value.length > max ? ` Máximo de ${max} digitos` : undefined;
 const minLength = (value, min) =>
-    value && value.length < min ? ` O tamanho mínimo para este campo é ${min}` : undefined;
+  value && value.length < min ? ` Mínimo de ${min} digitos` : undefined;
 
-const nameText = (value) =>
-    value && !/[a-zA-ZÀ-ÿ]+\ +[a-zA-ZÀ-ÿ]/.test(value) ? ' Deve digitar o nome completo' : undefined;
+const Limite = (value, min, max) =>
+  value && (value < min || value > max) ? ` Deve estar entre ${min} e ${max}` : undefined;
+
+const nameText = value =>
+  value && !/[a-zA-ZÀ-ÿ]+\ +[a-zA-ZÀ-ÿ]/.test(value)
+    ? " Deve digitar o nome completo"
+    : undefined;
 const email = value =>
-    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Endereço de e-mail inválido' : undefined;
-const cpf = (value) => {
-    let origCPF = value.replace(/\D/g, '');
-    let Soma;
-    let Resto;
-    let i;
-    Soma = 0;
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? "Endereço de e-mail inválido"
+    : undefined;
+const cpf = value => {
+  let origCPF = value.replace(/\D/g, "");
+  let Soma;
+  let Resto;
+  let i;
+  Soma = 0;
 
-    for (i = 1; i <= 9; i++)
-        Soma = Soma + parseInt(origCPF.substring(i - 1, i)) * (11 - i);
+  for (i = 1; i <= 9; i++)
+    Soma = Soma + parseInt(origCPF.substring(i - 1, i)) * (11 - i);
 
-    Resto = (Soma * 10) % 11;
+  Resto = (Soma * 10) % 11;
 
-    if ((Resto == 10) || (Resto == 11))
-        Resto = 0;
-    if (Resto != parseInt(origCPF.substring(9, 10)))
-        return "CPF invalido";
+  if (Resto == 10 || Resto == 11) Resto = 0;
+  if (Resto != parseInt(origCPF.substring(9, 10))) return "CPF invalido";
 
-    Soma = 0;
-    for (i = 1; i <= 10; i++)
-        Soma = Soma + parseInt(origCPF.substring(i - 1, i)) * (12 - i);
+  Soma = 0;
+  for (i = 1; i <= 10; i++)
+    Soma = Soma + parseInt(origCPF.substring(i - 1, i)) * (12 - i);
 
-    Resto = (Soma * 10) % 11;
+  Resto = (Soma * 10) % 11;
 
-    if ((Resto == 10) || (Resto == 11))
-        Resto = 0;
-    if (Resto != parseInt(origCPF.substring(10, 11)))
-        return "CPF invalido";
+  if (Resto == 10 || Resto == 11) Resto = 0;
+  if (Resto != parseInt(origCPF.substring(10, 11))) return "CPF invalido";
 
-    return undefined;
-}
-
+  return undefined;
+};
 
 export function validarNome(value) {
-    let resp;
-    
-    resp = required(value);
-    if (resp)
-        return resp;
+  let resp;
 
-    resp = minLength(value, 8);
-    if (resp)
-        return resp;
+  resp = required(value);
+  if (resp) return resp;
 
-    resp = nameText(value);
-    if (resp)
-        return resp;
+  resp = minLength(value, 8);
+  if (resp) return resp;
+
+  resp = nameText(value);
+  if (resp) return resp;
 }
 
 export function validarEmail(value) {
-    let resp;
+  let resp;
 
-    resp = required(value);
-    if (resp)
-        return resp;
+  resp = required(value);
+  if (resp) return resp;
 
-    resp = email(value);
-    if (resp)
-        return resp;
+  resp = email(value);
+  if (resp) return resp;
 }
 
 export function validarTelefone(value) {
-    let resp;
-    
-    resp = required(value);
-    if (resp)
-        return resp;
+  let resp;
+  value = value.replace(/\D/g, "");
 
-    resp = defLength(value, 9);
-    if (resp)
-        return resp;
+  resp = required(value);
+  if (resp) return resp;
+
+  resp = minLength(value, 10);
+  if (resp) return resp;
 }
 
 export function validarCPF(value) {
+  let resp;
+  value = value.replace(/\D/g, "");
+
+  resp = required(value);
+  if (resp) return resp;
+
+  resp = defLength(value, 11);
+  if (resp) return resp;
+}
+
+export function validarNumero(value) {
     let resp;
-
+    value = value.replace(/\D/g, "");
+  
     resp = required(value);
-    if (resp)
-        return resp;
+    if (resp) return resp;
+  
+    resp = Limite(value, 1, 9999);
+    if (resp) return resp;
+  }
 
-    resp = defLength(value, 8);
-    if (resp)
-        return resp;    
+export function buscaCEP(cep) {
+  let endereco = { logradouro: "", bairro: "", cidade: "", estado: "" };
+  let error = '';
+  return new Promise((resolve, reject) => {
+    fetch("https://viacep.com.br/ws/" + cep + "/json/")
+      .then(resp => resp.json())
+      .then(function(data) {
+        if (Object.keys(data).length > 1) {
+          endereco.logradouro = data.logradouro;
+          endereco.bairro = data.bairro;
+          endereco.cidade = data.localidade;
+          endereco.estado = data.uf;
+        }
+        else
+            error = 'CEP invalido';
+        resolve({ error: error, endereco });
+      })
+      .catch(error => {
+        reject(error);
+        return;
+      });
+  });
+  /**/
 }
 
 export function validarCEP(value) {
-    let resp;
-    //let endereco = {logradouro:'', bairro: '', cidade: '', estado: ''};
+  value = value.replace(/\D/g, "");
+  //return new Promise((resolve, reject) => {
+  let resp;
+  let endereco = {
+    logradouro: "asdf",
+    bairro: "sdf",
+    cidade: "asdf",
+    estado: "MG"
+  };
 
-    resp = required(value);
-    if (resp)
-        //return {erro:resp, endereco};
-        return resp;
+  resp = required(value);
+  if (resp)
+    return resp;
 
-    resp = defLength(value, 14);
-    if (resp)
-        //return {erro:resp, endereco};
-        return resp;
-
-    resp = VerificaCEP(value);
-    if (resp)
-        //return {erro:resp, endereco};
-        return resp;
+  resp = defLength(value, 8);
+  if (resp)
+    return resp;
 }
-
+/*
 function VerificaCEP(value) {
-    //let endereco = {logradouro:'', bairro: '', cidade: '', estado: ''};
-    let cepValue = value.replace(/\D/g, '');
+  //let endereco = {logradouro:'', bairro: '', cidade: '', estado: ''};
+  let cepValue = value.replace(/\D/g, "");
 
-    if (cepValue.length == 8) {
-        
-        const https = require('https');
+  if (cepValue.length == 8) {
+    const https = require("https");
 
-        https.get('https://viacep.com.br/ws/' + cepValue + '/json/', (resp) => {
-            let data = '';
+    https
+      .get("https://viacep.com.br/ws/" + cepValue + "/json/", resp => {
+        let data = "";
 
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
+        // A chunk of data has been recieved.
+        resp.on("data", chunk => {
+          data += chunk;
+        });
 
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                data = JSON.parse(data);
-                console.log(data);
-                if (Object.keys(data).length > 1) {
-                    /*endereco.logradouro = data.logradouro;
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          data = JSON.parse(data);
+          console.log(data);
+          if (Object.keys(data).length > 1) {
+            /*endereco.logradouro = data.logradouro;
                     endereco.bairro = data.bairro;
                     endereco.cidade = data.localidade;
                     endereco.estado = data.uf;
-                    return {erro:'a', endereco};*/
-                    document.getElementById("logradouro").value = data.logradouro;
-                    document.getElementById("bairro").value = data.bairro;
-                    document.getElementById("cidade").value = data.localidade;
-                    document.getElementById("estado").value = data.uf;
-                    return "";
-                }
-                else {
-                    /*endereco.logradouro = "";
+                    return {erro:'a', endereco};/
+            document.getElementById("logradouro").value = data.logradouro;
+            document.getElementById("bairro").value = data.bairro;
+            document.getElementById("cidade").value = data.localidade;
+            document.getElementById("estado").value = data.uf;
+            return "";
+          } else {
+            /*endereco.logradouro = "";
                     endereco.bairro = "";
                     endereco.cidade = "";
                     endereco.estado = "";
-                    return {erro:'CEP invalido', endereco};*/
-                    document.getElementById("logradouro").value = "";
-                    document.getElementById("bairro").value = "";
-                    document.getElementById("cidade").value = "";
-                    document.getElementById("estado").value = "";
-                    return "CEP invalido";
-                }
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
+                    return {erro:'CEP invalido', endereco};/
+            document.getElementById("logradouro").value = "";
+            document.getElementById("bairro").value = "";
+            document.getElementById("cidade").value = "";
+            document.getElementById("estado").value = "";
+            return "CEP invalido";
+          }
         });
-    }
-    else {
-        /*endereco.logradouro = "";
+      })
+      .on("error", err => {
+        console.log("Error: " + err.message);
+      });
+  } else {
+    /*endereco.logradouro = "";
         endereco.bairro = "";
         endereco.cidade = "";
         endereco.estado = "";
-        return {erro:'a', endereco};*/
-        document.getElementById("logradouro").value = "";
-        document.getElementById("bairro").value = "";
-        document.getElementById("cidade").value = "";
-        document.getElementById("estado").value = "";
-        return "";
-    }
-};
+        return {erro:'a', endereco};/
+    document.getElementById("logradouro").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("estado").value = "";
+    return "";
+  }
+}
+*/
 /*import { api } from "../controllers/index";
 //import { FETCH_ALL_USUARIOS } from '../actions/types';
 
