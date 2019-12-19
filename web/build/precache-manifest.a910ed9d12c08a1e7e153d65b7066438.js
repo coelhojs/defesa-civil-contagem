8637a566a40fd9c1288ea1c7b7e53dedaedb6f81
coelhojs <ccoelho.me@gmail.com>
2019-12-19 11:38:03 -0300

    build

---
+++ web/build/precache-manifest.a910ed9d12c08a1e7e153d65b7066438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e636a756d7c03f7596cbae2f5ddfb902",
+    "url": "/defesa-civil-contagem/index.html"
+  },
+  {
+    "revision": "002978aa479ff8d118ec",
+    "url": "/defesa-civil-contagem/static/js/2.157b18bd.chunk.js"
+  },
+  {
+    "revision": "017b99723876250df126",
+    "url": "/defesa-civil-contagem/static/js/main.b0772056.chunk.js"
+  },
+  {
+    "revision": "1ca457ca77259bf1b35c",
+    "url": "/defesa-civil-contagem/static/js/runtime~main.dc8080ea.js"
+  },
+  {
+    "revision": "05fbaa3bab349ef824a6535315c790dc",
+    "url": "/defesa-civil-contagem/static/media/background.05fbaa3b.png"
+  },
+  {
+    "revision": "ff2997c2926411364cac184daebe97a5",
+    "url": "/defesa-civil-contagem/static/media/logo.ff2997c2.jpg"
+  }
+]);
